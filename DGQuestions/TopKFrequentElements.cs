424a514466f4745bd5ac129076bdbae311e9821f
knanda-degreed <knanda@degreed.com>
2024-11-01 22:26:28 +0530

    TopKFrequentElements - unoptimized and optimized

---
+++ DGQuestions/TopKFrequentElements.cs
@@ -0,0 +1,54 @@
+﻿namespace DGQuestions;
+
+/*
+Top K Frequent Elements
+Problem Description:
+Given an array of integers nums, return the k most frequent elements. You can assume that the array is non-empty and k is always valid.
+Input:
+nums[]: An array of integers.
+    k: An integer representing how many frequent elements to return.
+Output:
+Return an array of the k most frequent elements.
+    Example:
+Input:
+nums = [1, 1, 1, 2, 2, 3], k = 2
+Output:
+[1, 2]
+*/
+
+public class TopKFrequentElements : IDegreedSolution
+{
+    public void Run()
+    {
+        TopKFrequent([1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4], 2);
+    }
+
+    private void TopKFrequent(int[] input, int topKFrequent)
+    {
+        Console.WriteLine("Input to question: " + GetType());
+        Console.WriteLine("[" + string.Join(", ", input) + "]");
+
+        if (input.Length == 0)
+        {
+            return;
+        }
+
+        var result = new List<int>();
+        var frequencyMap = new Dictionary<int, int>();
+
+        foreach (var num in input)
+        {
+            if (!frequencyMap.TryAdd(num, 1))
+            {
+                frequencyMap[num]++;
+            }
+        }
+
+        var sortedFrequencyMap = frequencyMap.OrderByDescending(x => x.Value).Take(topKFrequent)
+            .Select(x => x.Key)
+            .ToArray();
+
+        Console.WriteLine("Output to question: " + GetType());
+        Console.WriteLine("[" + string.Join(", ", sortedFrequencyMap) + "]");
+    }
+}
---
+++ DGQuestions/TopKFrequentElementsMinHeap.cs
@@ -0,0 +1,51 @@
+﻿namespace DGQuestions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopKFrequentElementsMinHeap : IDegreedSolution
+{
+    public void Run()
+    {
+        var result = TopKFrequent(new int[] {1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4}, 2);
+        Console.WriteLine("Output: [" + string.Join(", ", result) + "]");
+    }
+
+    private int[] TopKFrequent(int[] input, int k)
+    {
+        Console.WriteLine("Input: [" + string.Join(", ", input) + "]");
+
+        if (input.Length == 0)
+        {
+            return [];
+        }
+
+        // Step 1: Build the frequency map
+        var frequencyMap = new Dictionary<int, int>();
+        foreach (var num in input)
+        {
+            if (!frequencyMap.TryAdd(num, 1))
+            {
+                frequencyMap[num]++;
+            }
+        }
+
+        // Step 2: Use a min-heap to keep track of the top k elements
+        var minHeap = new SortedSet<(int frequency, int number)>();
+        
+        foreach (var entry in frequencyMap)
+        {
+            minHeap.Add((entry.Value, entry.Key));
+
+            // If the heap size exceeds k, remove the element with the smallest frequency
+            if (minHeap.Count > k)
+            {
+                minHeap.Remove(minHeap.Min);
+            }
+        }
+
+        // Step 3: Extract the elements from the heap
+        return minHeap.Select(x => x.number).ToArray();
+    }
+}
