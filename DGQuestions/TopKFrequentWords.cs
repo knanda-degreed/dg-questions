2ce2586a6a982064fddf84534e9dcd271ec931ab
knanda-degreed <knanda@degreed.com>
2024-11-04 14:21:02 +0530

    Correcting TopKFrequentWords - adding problem statement

--- DGQuestions/TopKFrequentWords.cs
+++ DGQuestions/TopKFrequentWords.cs
@@ -1,5 +1,23 @@
 namespace DGQuestions;
 
+/*
+Top-K Frequent Words
+Problem Description:
+You are given a large dataset of words. Your goal is to find the top K most frequent words efficiently.
+    Input:
+words[]: List of words (e.g., ["machine", "learning", "data", "machine", "ai", "data"]).
+    k: An integer representing the number of top frequent words to return.
+Output:
+Return a list of the top K frequent words.
+    Example:
+Input:
+words = ["machine", "learning", "data", "machine", "ai", "data"], k = 2
+Output:
+["data", "machine"]
+*/
+
+// if there is a tie in frequency, the word with the lower alphabetical order comes first
+// e.g. apples is in the list twice, but work is in the list twice as well. apples should come first and thus not be in the output
 public class TopKFrequentWords : IDegreedSolution
 {
     public void Run()
